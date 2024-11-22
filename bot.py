268b47f17488a4e4f93ba8017bb60b3fde259fab
YungKillua <146393067+YungKillua@users.noreply.github.com>
2024-11-22 15:42:37 +0100

    Add files via upload

--- bot.py
+++ bot.py
@@ -489,7 +489,7 @@ def create_subprocess(order, tp, order_type):
     if systemos == 'win32':
         subprocess.Popen(f'start powershell -NoExit -Command "python {os.path.join(script_dir, script)} {var1} {var2} {var3} {var4}"', shell=True)
     elif systemos == 'linux':
-        subprocess.Popen(f'gnome-terminal -- bash -c "python3 {os.path.join(script_dir, script)} {var1} {var2} {var3} {var4}; exec bash"', shell=True)
+        subprocess.Popen(f'lxterminal -e "python3 {os.path.join(script_dir, script)} {var1} {var2} {var3} {var4}"', shell=True)
 
 def process_data():
     global received_data
